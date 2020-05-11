de88b6f6fbed1e7990c302709db6d8a8a747e27c
Chikeobi <chikeobi.njaka@gmail.com>
2020-05-11 12:33:00 -0700

    Final Mod...

--- app.js
+++ app.js
@@ -5,11 +5,28 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const teamHTML = path.join(__dirname, "team.html");
+const indexHTML = path.join(__dirname, "index.html");
+const styleCSS = path.join(__dirname, "style.css");
+
+const teamHTMLOutput = path.join(OUTPUT_DIR, "team.html");
+const indexHTMLOutput = path.join(OUTPUT_DIR, "index.html");
+const styleCSSOutput = path.join(OUTPUT_DIR, "style.css");
+
 const render = require("./lib/htmlRenderer");
 const promptSync = require("prompt-sync")();
 const utilities = require("./lib/utilities");
-const logIt = true;
+const logIt = false;
+
+const STYLE = `.team-heading{
+  background-color: #e64a58;
+}
+
+.card-header{
+  background-color: #147bf3;
+}
+`;
 
 function replacePlaceHolder(htmlTemplate, placeholder, replacement) {
   let retval = null;
@@ -35,23 +52,40 @@ function generateEmployeeId() {
   return Math.floor((Math.random() * Date.now()) / 1000000);
 }
 
-const internRoles = ["Clerical", "Research", "Gofer", "Social Media", "Code Formatter", "Intern"];
+const internRoles = [
+  "Clerical Intern",
+  "Research Intern",
+  "Gofer Intern",
+  "Social Media Intern",
+  "Code Formatter Intern",
+  "Intern",
+];
 
-const managerRoles = ["Project", "Finance", "HR", "Engineering", "Production", "Test", "Software", "Manager"];
+const managerRoles = [
+  "Project Manager",
+  "Finance Manager",
+  "HR Manager",
+  "Engineering Manager",
+  "Production Manager",
+  "Test Manager",
+  "Software Manager",
+  "Manager",
+];
 
-const engineerRoles = ["Junior", "Senior", "Development", "Database", "Architect", "Engineer"];
+const engineerRoles = [
+  "Junior Engineer",
+  "Senior Engineer",
+  "Development Engineer",
+  "Database Engineer",
+  "Data Architect",
+  "Engineer",
+];
 
 const employeeQuestions = [
   {
     name: "name",
     type: "input",
     message: "Enter Name: ",
-  },
-  {
-    name: "id",
-    type: "input",
-    message: "Enter ID:",
-    default: utilities.generateEmployeeId(),
   },
   {
     name: "email",
@@ -62,6 +96,12 @@ const employeeQuestions = [
 
 const internQuestions = [
   ...employeeQuestions,
+  {
+    name: "id",
+    type: "input",
+    message: "Enter ID:",
+    default: utilities.generateEmployeeId(),
+  },
   {
     name: "school",
     type: "input",
@@ -80,6 +120,12 @@ const internQuestions = [
 const engineerQuestions = [
   ...employeeQuestions,
   {
+    name: "id",
+    type: "input",
+    message: "Enter ID:",
+    default: utilities.generateEmployeeId(),
+  },
+  {
     name: "github",
     type: "input",
     message: "Github ID? ",
@@ -96,6 +142,12 @@ const engineerQuestions = [
 const managerQuestions = [
   ...employeeQuestions,
   {
+    name: "id",
+    type: "input",
+    message: "Enter ID:",
+    default: utilities.generateEmployeeId(),
+  },
+  {
     name: "role",
     type: "list",
     message: "Choose Manager Role",
@@ -109,67 +161,95 @@ const managerQuestions = [
   },
 ];
 
-// async function internCall() {
-//   if (logIt) console.log("Calling Intern Inquire...\n\n");
-//   let intern = await inquirer.prompt(internQuestions);
-//   if (logIt) console.log(intern);
-//   let intrn = new Intern(intern.name, intern.id, intern.email, intern.school, intern.role);
-//   data["intern"].push(intrn);
-//   if (logIt) console.log(data);
-//   if (intern.again) internCall();
-//   else return;
-// }
+function renderHTML(data) {
+  if (data == null) return;
+  let manager = data["manager"];
+  let div = managerTemplate;
+  let divs = "";
+  div = utilities.replacePlaceHolder(div, "name", manager.getName());
+  div = utilities.replacePlaceHolder(div, "id", manager.getId());
+  div = utilities.replacePlaceHolder(div, "role", manager.getRole());
+  div = utilities.replacePlaceHolder(div, "email", manager.getEmail());
+  div = utilities.replacePlaceHolder(div, "officeNumber", manager.getOfficeNumber());
+  divs += div;
+  if (logIt) console.log(div);
+  let members = null;
+  if ((members = data["engineers"]) != null) {
+    for (let index = 0; index < members.length; index++) {
+      let engineer = members[index];
+      div = utilities.replacePlaceHolder(engineerTemplate, "name", engineer.getName());
+      div = utilities.replacePlaceHolder(div, "id", engineer.getId());
+      div = utilities.replacePlaceHolder(div, "role", engineer.getRole());
+      div = utilities.replacePlaceHolder(div, "email", engineer.getEmail());
+      div = utilities.replacePlaceHolder(div, "github", engineer.getGithub());
+      divs += div;
+    }
+  }
+  if ((members = data["interns"]) != null) {
+    for (let index = 0; index < members.length; index++) {
+      let intern = members[index];
+      div = utilities.replacePlaceHolder(internTemplate, "name", intern.getName());
+      div = utilities.replacePlaceHolder(div, "id", intern.getId());
+      div = utilities.replacePlaceHolder(div, "role", intern.getRole());
+      div = utilities.replacePlaceHolder(div, "email", intern.getEmail());
+      div = utilities.replacePlaceHolder(div, "school", intern.getSchool());
+      divs += div;
+    }
+  }
+  let main = utilities.replacePlaceHolder(mainTemplate, "team", divs);
+  if (logIt) console.log(main);
+  // write to output(s)
+  fs.writeFileSync(teamHTML, main);
+  fs.writeFileSync(indexHTML, main);
+  fs.writeFileSync(styleCSS, STYLE);
 
-// async function engineerCall() {
-//   if (logIt) console.log("Calling Intern Inquire...\n\n");
-//   let engineer = await inquirer.prompt(engineerQuestions);
-//   if (logIt) console.log(engineer);
-//   let engr = new Intern(intern.firstName, intern.lastName, intern.role, intern.email, intern.school);
-//   data["engineer"].push(engr);
-//   if (logIt) console.log(data);
-//   if (engineer.again) engineerCall();
-//   else return;
-// }
+  fs.writeFileSync(teamHTMLOutput, main);
+  fs.writeFileSync(indexHTMLOutput, main);
+  fs.writeFileSync(styleCSSOutput, STYLE);
+}
 
 let data = {};
 async function dataCollectionCall() {
-  if (logIt) console.log("Calling Manager Inquire...\n\n");
+  console.log("Collect Team Manager Information...\n\n");
+  console.log("==".repeat(20));
   let manager = await inquirer.prompt(managerQuestions);
   if (logIt) console.log(manager);
   let mgr = new Manager(manager.name, manager.id, manager.email, manager.officeNumber, manager.role);
   data["manager"] = mgr;
   if (logIt) console.log(data);
 
+  console.log("Collect Team Intern(s) Information...\n\n");
   let ans = promptSync("Add Interns to the team (y/n) Y?");
   console.clear();
-  data["interns"] = [];
   if (ans == null || ans == "") ans = "y";
   while (ans.charAt(0) !== "n") {
-    console.log("=".repeat(20));
+    console.log("==".repeat(20));
     let intern = await inquirer.prompt(internQuestions);
-    console.log(intern);
+    if (logIt) console.log(intern);
     let intrn = new Intern(intern.name, intern.id, intern.email, intern.school, intern.role);
-    console.log(intrn);
+    if (logIt) console.log(intrn);
     data["interns"].push(intrn);
     ans = promptSync("Add another intern to the team (y/n) Y?");
     if (ans == null || ans == "") ans = "y";
   }
-  console.log("\nAre we waiting?\n");
+  if (logIt) console.log(data);
 
+  console.log("Collect Team Engineer(s) Information...\n\n");
   ans = promptSync("Add Engineers to the team (y/n) Y?");
   if (ans == null || ans == "") ans = "y";
   console.clear();
-  data["engineers"] = [];
   while (ans.charAt(0) !== "n") {
-    console.log("=".repeat(20));
+    console.log("==".repeat(20));
     let engineer = await inquirer.prompt(engineerQuestions);
-    console.log(engineer);
+    if (logIt) console.log(engineer);
     let engr = new Engineer(engineer.name, engineer.id, engineer.email, engineer.github, engineer.role);
-    console.log(engr);
+    if (logIt) console.log(engr);
     data["engineers"].push(engr);
     ans = promptSync("Add another engineer to the team (y/n) Y?");
     if (ans == null || ans == "") ans = "y";
   }
+  if (logIt) console.log(data);
+  renderHTML(data);
 }
 
 let engineerTemplate = utilities.loadTemplate("./templates/engineer.html");
@@ -177,7 +257,7 @@ let internTemplate = utilities.loadTemplate("./templates/intern.html");
 let managerTemplate = utilities.loadTemplate("./templates/manager.html");
 let mainTemplate = utilities.loadTemplate("./templates/main.html");
 
-data["intern"] = [];
-data["engineer"] = [];
+data["interns"] = [];
+data["engineers"] = [];
 console.clear();
 dataCollectionCall();
